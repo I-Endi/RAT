610a901d1535b6f0bded6968bda995634e445dd9
Rodrigo <r.martin.nunez@student.tue.nl>
2022-05-15 15:33:06 +0200

    Created host file

--- src/host.py
+++ src/host.py
@@ -2,12 +2,15 @@ import os
 import socket,subprocess
 
 def main(ip: str, port: int):
+    file = open("output.txt", "wb")
+    
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server_address = (ip, port)
     s.bind(server_address)
     s.listen()
     conn, client_address = s.accept()
     data = conn.recv(15)
+    file.write(data)
     print(data)
 
 if __name__ == "__main__":
