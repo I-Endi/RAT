fd731396dcfaa3a6f15037990007b6881f22ffc8
Rodrigo Martín Núñez <r.martin.nunez@student.tue.nl>
2022-05-21 19:05:37 +0200

    Attempt of simple server listener

--- src/server_main.py
+++ src/server_main.py
@@ -1,36 +1,34 @@
-import socket
+from server.listen import Listener
 
 
 class Server:
     """
-    Not sure what this does lol
+    Starts the listener on the server
     """
 
-    def __init__(self, host: str, port: int, sock: socket) -> None:
+    def __init__(self, port: int) -> None:
         """
         Constructor
 
-        :param host: the server's address
-        :param port:
-        :param sock:
+        :param port: The server's port
         """
-        self.host = host
+        
         self.port = port
-        self.sock = sock
+        
+        #Create listener instance
+        self.Listener = Listener(self.port)
+
 
     def run(self) -> None:
         """
-        Endi pls explain this
+        Starts to listen for connections from client
         """
-
-        # Create file 'output.txt' and open int in writable binary
-        file = open("output.txt", "wb")
-
-        s = self.sock.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (self.host, self.port)
-        s.bind(server_address)
-        s.listen()
-        conn, client_address = s.accept()
-        data = conn.recv(15)
-        file.write(data)
-        print(data)
+        
+        self.Listener.listen()
+        
+        self.Listener.sock.close()
+        
+        
+if __name__ == "__main__":
+    # Run RAT with specified IP and port
+    Server(4444).run()
